8f9c6e8409258c96709511e7e46618c97ea9118a
YenduvaSrivalli <yenduvasrivalli2004@gmail.com>
2025-06-05 23:58:55 +0530

    Blog

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.locals.isActiveRoute = isActiveRoute; 
 
